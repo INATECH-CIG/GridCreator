884e5925c2fe1e4d2a501e2d86a49add6790d7d0
gugrimm <155527339+gugrimm@users.noreply.github.com>
2025-10-31 13:11:40 +0100

    adjust dict and generators aggregation

--- dicts.py
+++ dicts.py
@@ -13,8 +13,49 @@ solar_dict={
     'Nord': 180
 }
 
-generator_agg_dict = {'p_nom' : 'sum',
-                      'efficiency' : 'mean',}
+generator_agg_dict = {'bus': 'first',
+            'control' : 'last', # as first might be Slack
+            'type' : 'first',
+            'p_nom' : 'sum',
+            # 'p_nom_mod' : 'sum',
+            'p_nom_extendable' : 'first',
+            'p_nom_min' : 'sum', # only for capacity expansion
+            'p_nom_max' : 'sum',
+            'p_nom_set' : 'sum',
+            'p_min_pu' : 'mean', # small error: normally we should use weighted mean here
+            'p_max_pu' : 'mean', # same here
+            'p_set' : 'sum',
+            #'e_sum_min' : 'sum',
+            #'e_sum_max' : 'sum',
+            #'q_set' : 'sum', # ?
+            'sign' : 'first',
+            'carrier': 'first',
+            'marginal_cost' : 'mean',
+            'marginal_cost_quadratic' : 'sum',
+            'active' : 'first',
+            'build_year' : 'mean',
+            'lifetime' : 'mean',
+            'capital_cost' : 'mean',
+            'efficiency': 'mean',
+            # 'committable' : 'first',
+            # 'start_up_cost' : 'mean',
+            # 'shut_down_cost' : 'mean',
+            # 'stand_by_cost' : 'mean',
+            'min_up_time' : 'mean',
+            'min_down_time' : 'mean',
+            'up_time_before' : 'mean',
+            'down_time_before' : 'mean',
+            'ramp_limit_up' : 'mean',
+            'ramp_limit_down' : 'mean',
+            'ramp_limit_start_up' : 'mean',
+            'ramp_limit_shut_down' : 'mean',
+            'weight' : 'mean',
+            'p_nom_opt': 'sum',
+            'weather_cell_id': 'first',
+            'subtype': 'first',
+            'source_id': 'first',
+            'voltage_level': 'first',
+            }
 
 agg_dict={
     'Zensus_Holz_Holzpellets': 'sum',
