93fa4d655f16d1d06871a0e7ff70a5c0575d2804
gugrimm <155527339+gugrimm@users.noreply.github.com>
2025-11-03 15:58:13 +0100

    code to retrieve weather data

---
+++ weather_germany.py
@@ -0,0 +1,350 @@
+import cdsapi
+
+#%%
+
+"""
+Download weather data for Germany from Copernicus Data Store (CDS)
+"""
+
+
+"""
+Runtime of approx 97min for one year of data
+"""
+year = '2013'
+area = [56, 6, 47, 15] # Nord, West, Süd, Ost
+
+# cdsapi Client initialisieren
+client = cdsapi.Client()
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": "reanalysis",
+    "variable": "10m_u_component_of_wind",
+    "year": str(year),
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "format": "netcdf",
+    "area": area
+}
+
+# Anfrage an den CDS stellen und Daten speichern
+client.retrieve(dataset, request, "GER_10m_u_component_of_wind.nc")
+
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": "reanalysis",
+    "variable": "10m_v_component_of_wind",
+    "year": str(year),
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "format": "netcdf",
+    "area": area
+}
+
+# Anfrage an den CDS stellen und Daten speichern
+client.retrieve(dataset, request, "GER_10m_v_component_of_wind.nc")
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": "reanalysis",
+    "variable": "2m_dewpoint_temperature",
+    "year": str(year),
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "format": "netcdf",
+    "area": area
+}
+
+# Anfrage an den CDS stellen und Daten speichern
+client.retrieve(dataset, request, "GER_2m_dewpoint_temperature.nc")
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": "reanalysis",
+    "variable": "2m_temperature",
+    "year": str(year),
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "format": "netcdf",
+    "area": area
+}
+
+# Anfrage an den CDS stellen und Daten speichern
+client.retrieve(dataset, request, "GER_2m_temperature.nc")
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": "reanalysis",
+    "variable": "surface_pressure",
+    "year": str(year),
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "format": "netcdf",
+    "area": area
+}
+
+# Anfrage an den CDS stellen und Daten speichern
+client.retrieve(dataset, request, "GER_surface_pressure.nc")
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": "reanalysis",
+    "variable": "surface_solar_radiation_downwards",
+    "year": str(year),
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "format": "netcdf",
+    "area": area
+}
+
+# Anfrage an den CDS stellen und Daten speichern
+client.retrieve(dataset, request, "GER_surface_solar_radiation_downwards.nc")
+
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": "reanalysis",
+    "variable": "total_sky_direct_solar_radiation_at_surface",
+    "year": str(year),
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "format": "netcdf",
+    "area": area
+}
+
+# Anfrage an den CDS stellen und Daten speichern
+client.retrieve(dataset, request, "GER_total_sky_direct_solar_radiation_at_surface.nc")
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": "reanalysis",
+    "variable": "total_cloud_cover",
+    "year": str(year),
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "format": "netcdf",
+    "area": area
+}
+
+# Anfrage an den CDS stellen und Daten speichern
+client.retrieve(dataset, request, "GER_total_cloud_cover.nc")
+
+
+# %%
