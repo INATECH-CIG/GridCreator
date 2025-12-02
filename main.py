dd2888417ac159dca27f9668aa405de4b43e6f84
gugrimm <155527339+gugrimm@users.noreply.github.com>
2025-12-02 10:39:12 +0100

    delete paper plot and old plots

--- main.py
+++ main.py
@@ -188,107 +188,21 @@ examples = {
     }
 }
 
-# grid, buses, bbox, area, features = GridCreator(top, bottom, left, right, scenario=scenario)
 
-# #%%
-# # Anzahl an Solaranlagen im Grid:
-# num_solar_gens = grid.generators[grid.generators['carrier'] == 'solar'].shape[0]
-# print(f"Anzahl an Solaranlagen im Netz: {num_solar_gens}")
+# %%
+# STEP 6
+# .optimize()
 
-# # Anzahl an StorgaeUnits im Grid:
-# num_storage_units = grid.storage_units.shape[0]
-# print(f"Anzahl an Speichersystemen im Netz: {num_storage_units}")
+# # Fix Capacity
+# grid_1.optimize.fix_optimal_capacities()
 
+# # Set snapshots für Optimierung
+# start_time = pd.Timestamp("2023-01-01 00:00:00")
+# end_time = pd.Timestamp("2023-01-07 23:00:00")
+# snapshots = pd.date_range(start=start_time, end=end_time, freq='h')
+# grid_1.set_snapshots(snapshots)
 
-# #%%
-# grid.export_to_netcdf("output/grid_Schallstadt_test.nc")
-
-# #%%
-# # buses.to_csv("output/buses_Schallstadt_GER.csv")
-
-# import networkx as nx
-# nx.write_gpickle(area, "output/area_Schallstadt_test.pkl") 
-
-# import geopandas as gpd
-# features.to_file("output/features_Schallstadt_GER.gpkg", driver="GPKG")
-
-
-# #%%
-# import matplotlib.pyplot as plt
-# import cartopy.crs as ccrs
-# import osmnx as ox
-
-# network = grid
-
-# fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': ccrs.PlateCarree()})
-# # Netzplot
-# network.plot(ax=ax, bus_sizes=1 / 2e9, margin=1000)
-# # OSM-Daten
-# ox.plot_graph(area, ax=ax, show=False, close=False)
-# features.plot(ax=ax, facecolor="khaki", edgecolor="black", alpha=0.1)
-# # Liste von Generator-Kategorien: (Filter-Funktion, Farbe, Label)
-# gen_categories = [
-#     (lambda g: g['carrier'] == 'solar', 'yellow', 'Solar Generatoren'),
-#     (lambda g: g['carrier'] == 'E_car', 'green', 'E-Car Generatoren'),
-#     (lambda g: g['carrier'] == 'HP', 'blue', 'HP Generatoren'),
-#     (lambda g: (g['carrier'] == 'HP') & (g['carrier'] == 'solar'), 'purple', 'HP & Solar Generatoren'),
-#     (lambda g: (g['carrier'] == 'HP') & (g['carrier'] == 'E_car'), 'pink', 'HP & E-Car Generatoren'),
-#     (lambda g: (g['carrier'] == 'solar') & (g['carrier'] == 'E_car'), 'violet', 'Solar & E-Car Generatoren')
-# ]
-
-# for filt, color, label in gen_categories:
-#     gens = network.generators[filt(network.generators)]
-#     if not gens.empty:
-#         buses = gens['bus'].unique()
-#         coords = network.buses.loc[buses, ['x', 'y']]
-#         ax.scatter(
-#             coords['x'], coords['y'],
-#             color=color,
-#             s=20,
-#             label=label,
-#             zorder=5
-#         )
-
-# # Trafo-Busse markieren
-# tra_buses = network.transformers['bus1'].unique()
-# tra_coords = network.buses.loc[tra_buses][['x', 'y']]
-# ax.scatter(
-#     tra_coords['x'],
-#     tra_coords['y'],
-#     color='red',
-#     s=10,         # Punktgröße
-#     label='Transformers',
-#     zorder=5      # überlagert andere Layer
-# )
-
-
-# ax.legend(loc='upper right')
-
-
-# #%%
-# grid.export_to_netcdf("input/dist_grid_for_optimize.nc")
-
-# import networkx as nx
-# nx.write_gpickle(area, "input/area_for_optimize.pkl") 
-
-# import geopandas as gpd
-# features.to_file("input/features_for_optimize.gpkg", driver="GPKG")
-
-# # %%
-# # STEP 6
-# # .optimize()
-
-# # # Fix Capacity
-# # grid_1.optimize.fix_optimal_capacities()
-
-# # # Set snapshots für Optimierung
-# # start_time = pd.Timestamp("2023-01-01 00:00:00")
-# # end_time = pd.Timestamp("2023-01-07 23:00:00")
-# # snapshots = pd.date_range(start=start_time, end=end_time, freq='h')
-# # grid_1.set_snapshots(snapshots)
-
-# #
-# # grid_1.optimize()
+# grid_1.optimize()
 # %%
 scenario = "Opfingen"
 steps = [1,2,3,4,5]
@@ -330,31 +244,31 @@ if __name__ == "__main__":
     When the grid is reloaded, the time series are no longer available; the data frame is instead filled with NaN values.
     Plot Step4 therefore only works if the grid is still in memory after creation.
     '''
-    # grid = pypsa.Network(os.path.join('output', scenario, 'step_5', 'grid.nc'))
+    grid = pypsa.Network(os.path.join('output', scenario, 'step_5', 'grid.nc'))
     area = gpd.read_file(os.path.join('output', scenario, 'step_5', 'area.gpkg'))
     features = gpd.read_file(os.path.join('output', scenario, 'step_5', 'features.gpkg'))
     buses = pd.read_csv(os.path.join('output', scenario, 'step_5', 'buses.csv'), index_col=0)
     zensus_path = os.path.join(os.getcwd(), 'input', 'zensus_daten', 'Zensus2022_Durchschn_Nettokaltmiete_Anzahl_der_Wohnungen_100m-Gitter.csv')
 
-    # fig, ax = basic_plotting.plot_step1(grid, figsize=(10,10), legend_loc='upper left', bool_gridlinelabels=True)
-    # if save_plots:
-    #     fig.savefig(os.path.join('output', scenario, 'step1_grid.png'), dpi=300)
-    #     fig.savefig(os.path.join('output', scenario, 'step1_grid.pdf'))
+    fig, ax = basic_plotting.plot_step1(grid, figsize=(10,10), legend_loc='upper left', bool_gridlinelabels=True)
+    if save_plots:
+        fig.savefig(os.path.join('output', scenario, 'step1_grid.png'), dpi=300)
+        fig.savefig(os.path.join('output', scenario, 'step1_grid.pdf'))
 
     zensus_feature = "durchschnMieteQM"
     zensus_feature_nicename = r"Average Rent in €/$m^2$"
-    # fig, ax = basic_plotting.plot_step2(grid, features, buses, zensus_path, zensus_feature=zensus_feature,
-    #                                     zensus_feature_nicename=zensus_feature_nicename, figsize=(10,10), legend_loc='upper left', bool_gridlinelabels=False)
-    # if save_plots:
-    #     fig.savefig(os.path.join('output', scenario, f'step2_zensus_{zensus_feature}.png'), dpi=300)
-    #     fig.savefig(os.path.join('output', scenario, f'step2_zensus_{zensus_feature}.pdf'))
+    fig, ax = basic_plotting.plot_step2(grid, features, buses, zensus_path, zensus_feature=zensus_feature,
+                                        zensus_feature_nicename=zensus_feature_nicename, figsize=(10,10), legend_loc='upper left', bool_gridlinelabels=False)
+    if save_plots:
+        fig.savefig(os.path.join('output', scenario, f'step2_zensus_{zensus_feature}.png'), dpi=300)
+        fig.savefig(os.path.join('output', scenario, f'step2_zensus_{zensus_feature}.pdf'))
 
-    # fig, ax = basic_plotting.plot_step3(grid, features, buses, zensus_path, zensus_feature=zensus_feature,
-    #                                     zensus_feature_nicename=zensus_feature_nicename, figsize=(10,10), bool_legend=True, legend_loc='upper left',
-    #                                     plot_trafos=True)
-    # if save_plots:
-    #     fig.savefig(os.path.join('output', scenario, f'step3_technologies_{zensus_feature}.png'), dpi=300)
-    #     fig.savefig(os.path.join('output', scenario, f'step3_technologies_{zensus_feature}.pdf'))
+    fig, ax = basic_plotting.plot_step3(grid, features, buses, zensus_path, zensus_feature=zensus_feature,
+                                        zensus_feature_nicename=zensus_feature_nicename, figsize=(10,10), bool_legend=True, legend_loc='upper left',
+                                        plot_trafos=True)
+    if save_plots:
+        fig.savefig(os.path.join('output', scenario, f'step3_technologies_{zensus_feature}.png'), dpi=300)
+        fig.savefig(os.path.join('output', scenario, f'step3_technologies_{zensus_feature}.pdf'))
 
 
 
@@ -363,104 +277,3 @@ if __name__ == "__main__":
         fig.savefig(os.path.join('output', scenario, f'step4_ts.png'), dpi=300)
         fig.savefig(os.path.join('output', scenario, f'step4_ts.pdf'))
 
-# %%
-# grid.generators nach bus  gruppieren
-grupped = grid.generators.groupby('bus')
-# als dict speichern
-gen_dict = {bus: grupped.get_group(bus) for bus in grupped.groups}
-
-# keys mti mehr als 1 generator
-keys_multiple_gens = [key for key, value in gen_dict.items() if len(value) > 1]
-
-print(f'index für solar und hp generatoren an selben bus: solar: {gen_dict[keys_multiple_gens[0]].index[0]}, hp: {gen_dict[keys_multiple_gens[0]].index[1]}')
-
-
-# %%
-import matplotlib.pyplot as plt
-figsize = (10, 5)
-xlabel = 'Time in hours'
-ylabel = 'Power in W'
-gridlines = True
-
-# Am 1. Mai starten
-begin = 24*120  # 120 Tage * 24 Stunden
-end = begin + 24*7  # 1 Woche
-
-
-bus = 'BranchTee_mvgd_32594_lvgd_3031000002_building_727415'
-# bus random auswählen
-solar = f'{bus}_solar'
-hp = f'{bus}_HP'
-
-# plot von Load
-fig, ax = plt.subplots(figsize=figsize)
-(grid.loads_t.p_set[f'{bus}_load_1'].iloc[begin:end]*1e6).plot(ax=ax, label='Load', color='green')
-# Plot of only one week
-(grid.generators_t.p_max_pu[solar][begin:end]*grid.generators.at[solar, 'p_nom']*1e6).plot(ax=ax, label=f'Solar Generator', linestyle='-', color='orange')
-
-(grid.generators_t.p_max_pu[hp][begin:end]*grid.generators.at[hp, 'p_nom']*1e3).plot(ax=ax, label=f'Heat Pump', linestyle='-', color='blue')
-if gridlines:
-    # Stil des Gitternetzes (kann nach Geschmack angepasst werden)
-    ax.grid(
-
-    )
-
-    # Wichtig: Gitternetz **unter** die Daten legen
-    ax.set_axisbelow(True)
-title = None
-if title is not None:
-    plt.title(title)
-# grid hinterlegen
-#plt.grid()
-plt.xlabel(xlabel)
-plt.ylabel(ylabel)
-plt.legend()
-plt.tight_layout()
-
-#%%
-# als pdf speichern
-fig.savefig(os.path.join('output', scenario, f'step4_ts_example_load_solar_hp.pdf'))
-# als png speichern
-
-# %%
-figsize = (10, 5)
-xlabel = 'Time in hours'
-ylabel = 'Power in W'
-gridlines = True
-import matplotlib.pyplot as plt
-begin = 0
-grupped = grid.generators.groupby('bus')
-# als dict speichern
-gen_dict = {bus: grupped.get_group(bus) for bus in grupped.groups}
-
-# keys mti mehr als 1 generator
-keys_multiple_gens = [key for key, value in gen_dict.items() if len(value) > 1]
-
-print(f'index für solar und hp generatoren an selben bus: solar: {gen_dict[keys_multiple_gens[0]].index[0]}, hp: {gen_dict[keys_multiple_gens[0]].index[1]}')
-
-
-# for one week
-end = begin + 24*7
-
-bus =  gen_dict[keys_multiple_gens[0]]
-# bus random auswählen
-solar = gen_dict[keys_multiple_gens[0]].index[0]
-hp = gen_dict[keys_multiple_gens[0]].index[1]
-# plot von Load
-fig, ax = plt.subplots(figsize=figsize)
-(grid.loads_t.p_set[f'{bus}_load_1'].iloc[begin:end]*1e6).plot(ax=ax, label='Load', color='green')
-# Plot of only one week
-(grid.generators_t.p_max_pu[solar][begin:end]*grid.generators.at[solar, 'p_nom']*1e6).plot(ax=ax, label=f'Solar Generator', linestyle='-', color='orange')
-
-(grid.generators_t.p_max_pu[hp][begin:end]*grid.generators.at[hp, 'p_nom']*1e3).plot(ax=ax, label=f'Heat Pump', linestyle='-', color='blue')
-if gridlines:
-    ax.grid()
-if title is not None:
-    plt.title(title)
-
-plt.grid()
-plt.xlabel(xlabel)
-plt.ylabel(ylabel)
-plt.legend()
-plt.tight_layout()
-# %%
